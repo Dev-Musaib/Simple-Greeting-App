ed7094ef081d8c2d480af513934f0d0881559027
Musaib <atomicsight809@gmail.com>
2020-05-02 18:45:19 +0530

    Added a validation feature

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
-const form = document.getElementById('myform')
-const cover = document.querySelector('.cover')
-const block = document.querySelector('.modal')
+const form = document.getElementById("myform");
+const cover = document.querySelector(".cover");
+const block = document.querySelector(".modal");
 let inp = document.querySelector('form input[type="text"]');
-const exitBtn = document.querySelector('.exitSpan')
-let newElement = document.createElement('h2');
+const exitBtn = document.querySelector(".exitSpan");
+let newElement = document.createElement("h2");
 
-
-form.addEventListener('submit', greet)
-exitBtn.addEventListener('click', exit)
+form.addEventListener("submit", greet);
+exitBtn.addEventListener("click", exit);
 
 function greet(e) {
+  if (inp.value === "") {
+    error();
+  } else {
+    block.style.transform = "scale(1)";
 
-    e.preventDefault();
-    block.style.transform = 'scale(1)'
+    cover.classList.add("active");
 
-    cover.classList.add('active')
-
-    newElement.className = 'modal-title sub-title'
+    newElement.className = "modal-title sub-title";
 
     let greeting = document.createTextNode(`Hello ${inp.value}`);
 
-    newElement.appendChild(greeting)
+    newElement.appendChild(greeting);
 
-    block.appendChild(newElement)
-
+    block.appendChild(newElement);
+  }
+  e.preventDefault();
 }
 
 function exit(e) {
+  newElement.textContent = "";
+  block.style.transform = "scale(0)";
+  cover.classList.remove("active");
+  cover.classList.add("cover");
+}
 
-    newElement.textContent = ''
-    block.style.transform = 'scale(0)'
-    cover.classList.remove('active');
-    cover.classList.add('cover')
+function error() {
+  inp.style.border = "1px solid red";
+  document.querySelector(".errorMessage").textContent =
+    "Please fill in the field";
 
-
-
+  setTimeout(function () {
+    inp.style.border = "none";
+    document.querySelector(".errorMessage").textContent = "";
+  }, 2000);
 }
